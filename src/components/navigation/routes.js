32b78736f6e14968dfb6a67f4097fac73ad3c686
Daniel Levesque <dlevesque@mirego.com>
2017-03-30 23:44:23 -0400

    Add new Save session class Deactivate save and navigate to page

--- src/components/navigation/routes.js
+++ src/components/navigation/routes.js
@@ -2,12 +2,14 @@ import Sessions from 'components/sessions';
 import Session from 'components/session';
 import RecordSession from 'components/record-session';
 import AddComment from 'components/add-comment';
+import SaveSession from 'components/save-session';
 
 const Routes = {
   Sessions: { screen: Sessions },
   RecordSession: {screen: RecordSession},
   Session: {screen: Session},
-  AddComment: {screen: AddComment}
+  AddComment: {screen: AddComment},
+  SaveSession: {screen: SaveSession}
 };
 
 export default Routes;
