8d4821b31c6d5d5fc49c3323cefb99296fc5244f
martingagnon <mgagnon@mirego.com>
2017-04-21 01:33:42 -0400

    Feature/splash (#27)
    * Fix small stuff
    
    * Add Splash + Login as a root view
    
    * Add Splash screen

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {Provider, connect} from 'react-redux';
 import {addNavigationHelpers} from 'react-navigation';
 import {AppNavigator} from 'components/navigation';
+import {AccessToken} from 'react-native-fbsdk';
+import Splash from 'components/splash';
+import Login from 'components/login';
 
 import configureStore from './store';
 
@@ -13,7 +16,22 @@ const store = configureStore(navReducer);
 
 @connect(state => ({nav: state.nav}))
 class AppWithNavigationState extends Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount() {
+    this.validateAccessToken();
+  }
+
+  async validateAccessToken() {
+    const accessToken = await AccessToken.getCurrentAccessToken();
+    this.setState({tokenValid: !!accessToken});
+  }
+
+  appNavigator() {
     return (
       <AppNavigator
         navigation={addNavigationHelpers({
@@ -22,6 +40,29 @@ class AppWithNavigationState extends Component {
         })}
       />
     );
+  }
+
+  splash() {
+    return (
+      <Splash/>
+    );
+  }
+
+  login() {
+    return (
+      <Login onLogin={() => this.validateAccessToken()}/>
+    );
+  }
+
+  render() {
+    const {tokenValid} = this.state;
+    if (tokenValid === true) {
+      return this.appNavigator();
+    } else if (tokenValid === false) {
+      return this.login();
+    } else {
+      return this.splash();
+    }
   }
 }
 
