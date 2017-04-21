b801225c4fa4514786954298a0b540afa6badeb1
Martin Gagnon <mgagnon@mirego.com>
2017-04-21 01:10:43 -0400

    Add Splash + Login as a root view

---
+++ src/components/splash/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Container from 'ui/container';
+
+export default class Splash extends Component {
+  static navigationOptions = {
+    title: 'Band Sessions'
+  };
+
+  render() {
+    return (
+      <Container></Container>
+    );
+  }
+}
